0b948c8a3502be9b00e9139642c4519a25d77701
Utkarsh Patil <utkarshp2305@gmail.com>
2019-06-22 19:16:06 +0530

    Revert "CSS , Email, Phone Number for know-your-council"

--- team/members_list/sync_list.py
+++
@@ -1,9 +0,0 @@
-from team.models import Member
-from django.core import serializers
-
-Member.objects.all().delete()
-
-
-with open('team/members_list/members.json') as data:
-        for deserialized_object in serializers.deserialize("json", data):
-            deserialized_object.save()
