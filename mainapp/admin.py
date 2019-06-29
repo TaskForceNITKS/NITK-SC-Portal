4d136587db5051e8b0ca7f0f08766db48380fc73
unknown <ankit.171it209@nitk.edu.in>
2019-06-29 11:48:32 +0530

    Meeting, Document Model added

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,13 +1,33 @@
 from django.contrib import admin
 
+
+from db_file_storage.form_widgets import DBAdminClearableFileInput
+from django import forms
+
 # Register your models here.
 from . import models
 
 class AnnouncementAdmin(admin.ModelAdmin):
     list_display=['text_string','time_of_announcement']
+class MeetingAdmin(admin.ModelAdmin):
+	list_display=['Agenda','time_of_meeting']
+'''class DocumentsAdmin(admin.ModelAdmin):
+	list_display=['name','the_file','type_of_doc']'''
+
+ #https://django-db-file-storage.readthedocs.io/en/latest/
+class DocumentsForm(forms.ModelForm):
+    class Meta:
+        model = models.Documents
+        exclude = []
+        widgets = {
+            'the_file': DBAdminClearableFileInput
+        }
+class DocumentsAdmin(admin.ModelAdmin):
+    form = DocumentsForm
 
 
 
 admin.site.register(models.Announcement,AnnouncementAdmin)
 admin.site.register(models.DocumentModel)
-admin.site.register(models.Documents)
+admin.site.register(models.Documents,DocumentsAdmin)
+admin.site.register(models.Meeting,MeetingAdmin)
